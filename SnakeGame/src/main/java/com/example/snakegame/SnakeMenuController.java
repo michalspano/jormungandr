f973fc4ad2bc96d54a002678520ddfe39cf49777
Malte Bengtsson <bmalte@student.chalmers.se>
2022-12-08 16:06:59 +0100

    update SnakeMenuController class
    Make use of the instantiator class

--- SnakeGame/src/main/java/com/example/snakegame/SnakeMenuController.java
+++ SnakeGame/src/main/java/com/example/snakegame/SnakeMenuController.java
@@ -1,11 +1,16 @@
+/***************************************************************************************************
+ * The Snake Game - Jörmungandr
+ * File: {@code SnakeMenuController.java}
+ * Members: Michal Spano, Malte Bengtsson, Simone Graziosi, Feride Hansson, Anna Mäkinen, Katinka Romanus
+ * For DIT094 Mini Project: Team Programming; SEM@GU.
+ ***************************************************************************************************/
+
 package com.example.snakegame;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
-
-import java.io.IOException;
 
 public class SnakeMenuController {
     @FXML
@@ -20,13 +25,13 @@ public class SnakeMenuController {
     InstantiateScenes instantiateScenes = new InstantiateScenes(); // composition
 
     @FXML
-    protected void startGame() throws IOException {
+    protected void startGame() {
         Stage stage = (Stage) menuGameScene.getScene().getWindow();
         instantiateScenes.instantiateGameScene(stage);
     }
 
     @FXML
     protected void exitGame() {
-        SnakeGameUtils.exitGameAlert(this.menuGameScene);
+        SnakeGameUtils.exitGameAlert(menuGameScene);
     }
 }
