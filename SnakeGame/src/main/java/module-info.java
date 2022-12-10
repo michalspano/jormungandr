fdfd8c8a465c1fadbc3db26d8eb62247a69b1a42
Malte Bengtsson <bmalte@student.chalmers.se>
2022-12-10 16:25:02 +0100

    Update Java module info
    Include snake subpackage and add JSONsupport.

--- SnakeGame/src/main/java/module-info.java
+++ SnakeGame/src/main/java/module-info.java
@@ -1,9 +1,19 @@
+/***************************************************************************************************
+ * The Snake Game - Jörmungandr
+ * File: {@code module-info.java} - the module-info file for the Snake Game
+ * Members: Michal Spano, Malte Bengtsson, Simone Graziosi, Feride Hansson, Anna Mäkinen, Katinka Romanus
+ * For DIT094 Mini Project: Team Programming; SEM@GU.
+ ***************************************************************************************************/
+
 module com.example.snakegame {
     requires javafx.controls;
     requires javafx.fxml;
 
     requires com.almasb.fxgl.all;
+    requires org.json;
 
     opens com.example.snakegame to javafx.fxml;
     exports com.example.snakegame;
+    exports com.example.snakegame.snake;
+    opens com.example.snakegame.snake to javafx.fxml;
 }
