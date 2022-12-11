53e52fe2ca618cb400e22d302e7f2bcb7838e0f6
Michal Spano <spano@student.chalmers.se>
2022-12-11 20:12:54 +0100

    Add `json` lib to `gradle` config

--- SnakeGame/src/main/java/com/example/snakegame/SnakeGameUtils.java
+++ SnakeGame/src/main/java/com/example/snakegame/SnakeGameUtils.java
@@ -1,11 +1,17 @@
-package com.example.snakegame;
+package com.example.snakegame.utils;
 
+import com.example.snakegame.snake.GridPiece;
 import javafx.event.ActionEvent;
 import javafx.scene.Node;
 import javafx.scene.Parent;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
+import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+
+import java.util.*;
 
 /**
  * This is a static class that contains utility methods for the Snake Game.
@@ -30,6 +36,78 @@ public class SnakeGameUtils {
             stage.close();
         }
     }
+    public static final Random random = new Random();
+
+    /**
+     * The constant images part of an anonymous class.
+     */
+    public static final Map<String, Image> images = new HashMap<>() {{
+        put("head", new Image(Objects.requireNonNull(getClass().getResource("images/snake_head.png")).toExternalForm()));
+        put("body", new Image(Objects.requireNonNull(getClass().getResource("images/snake_piece.png")).toExternalForm()));
+        put("enemyBody", new Image(Objects.requireNonNull(getClass().getResource("images/snake_piece_puffer.png")).toExternalForm()));
+        put("food", new Image(Objects.requireNonNull(getClass().getResource("images/puffer_fish.png")).toExternalForm()));
+        put("gameBackground", new Image(Objects.requireNonNull(getClass().getResource("images/gameBackground.png")).toExternalForm()));
+        put("enemy", new Image(Objects.requireNonNull(getClass().getResource("images/enemy.png")).toExternalForm()));
+        put("block", new Image(Objects.requireNonNull(getClass().getResource("images/block.png")).toExternalForm()));
+    }};
+
+    /**
+     * Detect collision over iterable boolean.
+     *
+     * @param x        the x
+     * @param y        the y
+     * @param iterable the iterable
+     * @return the boolean
+     */
+    public static boolean detectCollisionOverIterable(int x, int y, List<?> iterable) {
+        return iterable.stream().anyMatch(piece -> {
+            if (piece instanceof GridPiece gridPiece) {
+                return gridPiece.getX() == x && gridPiece.getY() == y;
+            }
+            return false;
+        });
+    }
+
+    /**
+     * Draw image.
+     *
+     * @param gc        the gc
+     * @param imageName the image name
+     * @param x         the x
+     * @param y         the y
+     * @param width     the width
+     * @param height    the height
+     */
+    public static void drawImage(GraphicsContext gc, String imageName, int x, int y, int width, int height) {
+        gc.drawImage(images.get(imageName), x, y, width, height);
+    }
+
+    /**
+     * Snake logger.
+     *
+     * @param snake the snake
+     */
+    public static void snakeLogger(List<GridPiece> snake) {
+        for (GridPiece sp : snake) { System.out.print(sp + " "); }
+        System.out.println(); // end line
+    }
+
+    /**
+     * Generate random RGB color.
+     *
+     * @return the color
+     */
+    public static Color generateRandomColor() {
+        final int COLOR_OFFSET = 50;
+
+        // range of each color is: COLOR_OFFSET to (255 - COLOR_OFFSET)
+        int red = COLOR_OFFSET + (int)(Math.random() * (255 - COLOR_OFFSET));
+        int green = COLOR_OFFSET + (int)(Math.random() * (255 - COLOR_OFFSET));
+        int blue = COLOR_OFFSET + (int)(Math.random() * (255 - COLOR_OFFSET));
+
+        return Color.rgb(red, green, blue);
+    }
+
 
     /**
      * Current scene string.
